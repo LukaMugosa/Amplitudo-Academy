e8f033c1bad290486aed9d98b3656b6eb55d28d6
Luka Mugosa <lukamugosa1945@gmail.com>
2020-04-17 18:28:59 +0200

    Adds users and roles tables

--- app/User.php
+++ app/User.php
@@ -36,4 +36,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin(){
+        $name = $this->role()->name;
+        return ($name === 'ROLE_ADMIN');
+    }
+    public function isMentor(){
+        $name = $this->role()->name;
+        return ($name === 'ROLE_MENTOR');
+    }
+    public function isSupervisor(){
+        $name = $this->role()->name;
+        return ($name === 'ROLE_SUPERVISOR');
+    }
+    public function isGuest(){
+        $name = $this->role()->name;
+        return ($name === 'ROLE_GUEST');
+    }
+    public function isStudent(){
+        $name = $this->role()->name;
+        return ($name === 'ROLE_STUDENT');
+    }
+
+    public function role(){
+        return $this->belongsTo('App\Post');
+    }
 }
